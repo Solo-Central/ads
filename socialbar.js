e366608d46e3d5b6e7f5adfeaa09a3a0c5ce34bf
qatual <roman@rxman.my>
2026-05-16 18:35:10 -0400

    aaaand i spelt useful wrong

--- socialbar.js
+++ socialbar.js
@@ -1,7 +1,7 @@
 (function() {
     var script = document.createElement('script');
     script.src = "https://throbbingimmensely.com/9b/33/6a/9b336a9bd19b2fadcf2b7b37d07d051f.js";
-    script.async = true; // this thingy allows the page load while still loading the script which is really usefull.  -qatual
+    script.async = true; // this thingy allows the page load while still loading the script which is really useful.  -qatual
     var head = document.head || document.getElementsByTagName('head')[0];
 
     if (head) {
